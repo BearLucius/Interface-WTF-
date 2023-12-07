a95d87dfc74df6e88f2cb811fe55926a58ca62b1
Krapivin <tailsforhire229@gmail.com>
2023-12-07 15:25:23 +0300

    v0.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,16 @@
 package org.example;
+import java.util.Scanner;
 public class Main {
-    public interface DopustimPrimer1 {
-    void primer1();
-    }
-
-    public interface DopustimPrimer2 {
-    String primer2(String c);
-    }
-
-    public interface DopustimPrimer3 {
-    double primer3(double a, double b);
-    }
-    public static void main(String[] args) {
-
+    public interface DopustimPrimer1 {void primer1();}
+    public interface DopustimPrimer2 {String primer2(String c);}
+//Double - Умножение;
+public interface DopustimPrimer3
+{double primer3(double a, double b);}
+    public static void main(String[] args)
+    {DopustimPrimer2 primer2;
         // Интерфейс с двумя значениями под две переменных;
         DopustimPrimer3 primer3;
         primer3 = (a,b)->a*b;
-        double res = primer3.primer3(5.4, 9);
-        System.out.println(res);
-        }
-    }
+        double res = primer3.primer3(5.4, 9.7);
+        System.out.println("Третий пример: " + res);}
+}
